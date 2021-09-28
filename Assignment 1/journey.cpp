7506813876fee2bfa0b193da2c04b1c861b9af7c
Drake C <dpcullen@mavs.coloradomesa.edu>
2021-09-28 09:18:27 -0600

    Add academic honesty statement

--- Assignment 1/journey.cpp
+++ Assignment 1/journey.cpp
@@ -4,6 +4,15 @@
     @author Drake Cullen
 */
 
+/**
+    I declare that all material in this assessment task is my work except where there is clear acknowledgement or reference to the work of others. 
+    I further declare that I have complied and agreed to the CMU Academic Integrity Policy at the University website.
+    http://www.coloradomesa.edu/student-services/documents
+    Author’s Name: Drake Cullen 
+    UID(700#): 700480375
+    Date: 9/28/2021
+*/
+
 #include "./include/Graph.h"
 
 
