0469a148a625acc2c4b807a2a75ff097a64741ee
Drake C <dpcullen@mavs.coloradomesa.edu>
2021-10-20 18:56:44 -0600

    Add academic integrity statement

--- Assignment3/ass3j.cpp
+++ Assignment3/ass3j.cpp
@@ -3,6 +3,15 @@
 #include <time.h>
 #include <chrono>
 
+/**
+    I declare that all material in this assessment task is my work except where there is clear acknowledgement or reference to the work of others. 
+    I further declare that I have complied and agreed to the CMU Academic Integrity Policy at the University website.
+    http://www.coloradomesa.edu/student-services/documents
+    Author’s Name: Drake Cullen 
+    UID(700#): 700480375
+    Date: 10/20/2021
+*/
+
 void readInput(string&, Hashtable &);
 void showMenu(Hashtable &hashTable, string &inputFile, string &outputFile, bool &displayMenu);
 void heapForTopOneFifty(Hashtable &, string &, bool);
